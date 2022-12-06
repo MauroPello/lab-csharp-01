3000bb3661e832757dee6777b2d7ce1e34b256ae
MauroPello <mauro.pellonara@outlook.com>
2022-12-07 00:18:46 +0100

    Finish 2-ComplexAlgebra

--- 2-ComplexAlgebra/Complex.cs
+++ 2-ComplexAlgebra/Complex.cs
@@ -1,22 +1,67 @@
+using System;
+
 namespace ComplexAlgebra
 {
     /// <summary>
     /// A type for representing Complex numbers.
     /// </summary>
     ///
-    /// TODO: Model Complex numbers in an object-oriented way and implement this class.
-    /// TODO: In other words, you must provide a means for:
-    /// TODO: * instantiating complex numbers
-    /// TODO: * accessing a complex number's real, and imaginary parts
-    /// TODO: * accessing a complex number's modulus, and phase
-    /// TODO: * complementing a complex number
-    /// TODO: * summing up or subtracting two complex numbers
     /// TODO: * representing a complex number as a string or the form Re +/- iIm
     /// TODO:     - e.g. via the ToString() method
-    /// TODO: * checking whether two complex numbers are equal or not
-    /// TODO:     - e.g. via the Equals(object) method
     public class Complex
     {
-        // TODO: fill this class\
+        public double Real { get; }
+        public double Imaginary { get; }
+
+        public Complex(double real, double imaginary) {
+            this.Real = real;
+            this.Imaginary = imaginary;
+        }
+
+        public double Modulus => Math.Sqrt(Real * Real + Imaginary * Imaginary);
+
+        // per evitare il NaN che ci sarebbe usando Math.Atan(Imaginary / Real)
+        public double Phase => Math.Atan2(Imaginary, Real);
+
+        public override string ToString() {
+            string real = this.Real == 0 ? "" : this.Real.ToString();
+            string sign = this.Imaginary > 0 ? "+ i" : "- i";
+            string imaginary = Math.Abs(this.Imaginary).ToString();
+
+            if(this.Real != 0) {
+                sign = this.Imaginary > 0 ? " + i" : " - i";
+            } else {
+                sign = this.Imaginary > 0 ? "i" : "-i";
+            }
+
+            if(Math.Abs(this.Imaginary) == 1 || this.Imaginary == 0) {
+                imaginary = "";
+                if(this.Imaginary == 0) {
+                    sign = "";
+                    if(this.Real == 0) {
+                        real = "0";
+                    }
+                }
+            }
+
+            return real + sign + imaginary;
+        }
+
+        protected bool Equals(Complex complex) => Real.Equals(complex.Real) && Imaginary.Equals(complex.Imaginary);
+
+        public override bool Equals(object obj) {
+            if (ReferenceEquals(null, obj)) return false;
+            if (ReferenceEquals(this, obj)) return true;
+            if (obj.GetType() != GetType()) return false;
+            return Equals((Complex) obj);
+        }
+
+        public override int GetHashCode() => HashCode.Combine(Real, Imaginary);
+
+        public Complex Complement() => new Complex(Real, -Imaginary);
+        
+        public Complex Plus(Complex complex) => new Complex(Real + complex.Real, Imaginary + complex.Imaginary);
+        
+        public Complex Minus(Complex complex) => new Complex(Real - complex.Real, Imaginary - complex.Imaginary);
     }
 }
